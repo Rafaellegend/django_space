c8b258a188f9b1c3524ae3e052d70c1bbf068835
Raffozo <rafaellegendofdark@hotmail.com>
2024-08-15 10:09:48 -0300

    refactoring - dynamic card with "for"

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request,'gallery/index.html')
+  dados = {
+  1: {"name": "Nebulosa de Carina",
+      "subtitle": "webbtelescope.org / NASA / James Webb"
+      },
+  2: {"name": "Galáxia NGC 1079",
+      "subtitle": "nasa.org / NASA / Hubble"
+      }
+}
+  return render(request,'gallery/index.html', {"cards": dados})
 
 def imagem(request):
   return render(request, 'gallery/imagem.html')
