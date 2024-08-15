04738e91934bc43a543ff8535cc27241507dca09
Raffozo <rafaellegendofdark@hotmail.com>
2024-08-15 10:10:30 -0300

    created photos table

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+  name = models.CharField(max_length=100, null=False, blank=False)
+  subtitle = models.CharField(max_length=150, null=False, blank=False)
+  description = models.TextField(null=False,blank=False)
+  foto = models.CharField(max_length=100, null=False, blank=False)
+
+  def __str__(self):
+    return f"Fotografia [nome = {self.name}]"
