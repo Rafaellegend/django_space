8d5aff5c1c1caaca393cbd46df66919bc27f3fea
Raffozo <rafaellegendofdark@hotmail.com>
2024-08-27 09:05:52 -0300

    barra de busca e pesquisa

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from gallery.views import index, imagem
+from gallery.views import buscar, index, imagem
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>' , imagem, name='imagem'),
+    path('buscar',buscar, name='buscar'),
 ]
